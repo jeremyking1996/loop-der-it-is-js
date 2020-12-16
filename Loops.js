158cecf34ff647e2362be4882b9e730d72493a7a
jeremyking1996 <73255406+jeremyking1996@users.noreply.github.com>
2020-12-16 08:40:30 -0500

    Cant figure out big loop

--- Loops.js
+++ Loops.js
@@ -1,7 +1,69 @@
 class LoopDeLoop {
+    oneToTen(){
+        let response ="";
+        for (let i=1; i <= 10; i++){
+            response += i+"\n";
+        }
+        return response;
+    }
+    loopsSquared(){
+        let answer ="";
+        for (let i =1; i <= 10; i++){
+        answer += (i * i) + "\n";
+        }
+        return answer;
+    }
+    evenUnder(n){
+        let output = "";
+        for (let i = 2; i < n; i+=2){
+        output += i+ "\n";
+        }
+        return output;
+    }
+    starLoop(){
+        let line, triangle;
+        line = triangle = "";
+        for (let row = 1; row <= 5; row++) {
+        line = "";
+        for (let x = 0; x < row; x++) {
+            line += "*";
+        }
+        triangle += line + "\n";
+        }
+        return triangle;
+    }
 
-
-
+    squareLoop(){
+       // print columns and row numbers
+       const maxTotalRows = 4;
+       const maxTotalColumns = 4;
+   let completedTable = "";
+   for (let row = 1; row <= maxTotalRows; row++) {
+       //OUTER LOOP
+       for (let column = 1; column <= maxTotalColumns; column++) {
+       //INNER LOOP
+       completedTable = completedTable + `|${row * column}`;
+       }
+       completedTable += "|\n";
+   }
+   return completedTable;
+   }
+   
+   bigSquareLoop(){
+    // print columns and row numbers
+    const maxTotalRows = 6;
+    const maxTotalColumns = 6;
+    let completedTable = "";
+    for (let row = 1; row <= maxTotalRows; row++) {
+    //OUTER LOOP
+    for (let column = 1; column <= maxTotalColumns; column++) {
+    //INNER LOOP
+    completedTable = completedTable + `|${row * column}`;
+    }
+    completedTable += "|\n";
+}
+return completedTable;
+}
 }
 
 
--- Loops.test.js
+++ Loops.test.js
@@ -1,3 +1,63 @@
 const { TestScheduler } = require('jest');
-const {Loops} = require('./Loops');
+const Loops = require('./Loops');
+
+test("test for one to ten",()=>{
+    //Given
+    let loops = new Loops();
+    let expected = "1\n2\n3\n4\n5\n6\n7\n8\n9\n10\n";
+    //When
+    let actual = loops.oneToTen();
+    //Then
+    expect(actual).toEqual(expected)
+})
+
+test("test for squared numbers", ()=>{
+        // Given
+        let loops = new Loops();
+        let expected =  "1\n4\n9\n16\n25\n36\n49\n64\n81\n100\n";
+        // When
+        let actual = loops.loopsSquared();
+        // Then
+        expect(actual).toEqual(expected);
+    }
+)
+
+test("test for even under", () => {
+        // Given
+        let loops = new Loops();
+        let expected = "2\n4\n6\n8\n10\n12\n14\n16\n18\n";
+        // When
+        let actual = loops.evenUnder(20)
+        // Then
+        expect(actual).toEqual(expected);
+})
+
+test("test for star pattern", () => {
+    // Given
+    let loops = new Loops();
+    let expected = "*\n**\n***\n****\n*****\n";
+    // When
+    let actual = loops.starLoop();
+    // Then
+    expect(actual).toEqual(expected);
+})
+
+test("test small multiplication table", () => {
+    //Given
+    let loops = new Loops();
+    let expected = "|1|2|3|4|\n|2|4|6|8|\n|3|6|9|12|\n|4|8|12|16|\n"
+    //When
+    let actual = loops.squareLoop();
+    //Then
+    expect(actual).toEqual(expected);
+})
+test("test big multiplication table", () => {
+    //Given
+    let loops = new Loops();
+    let expected = "|1|2|3|4|5|6|\n|2|4|6|8|10|12|\n|3|6|9|12|15|18|\n|4|8|12|16|20|24|\n|5|10|15|20|25|30|\n|6|12|18|24|30|36|"
+    //When
+    let actual = loops.bigSquareLoop();
+    //Then
+    expect(actual).toEqual(expected);
+})
 
